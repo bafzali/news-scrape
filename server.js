8f5dcfc82900ce79cab4b60e74d5bfcbae804bc5
Becky Afzali <beckyafzali@gmail.com>
2018-06-24 22:57:27 -0500

    fix port

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ mongoose.connect(MONGODB_URI);
 require('./controller/routes')(app);
 
 // Listen on port 3000
-app.listen(3000, function () {
-  console.log('App running on port 3000!');
-});
+app.listen(process.env.PORT || 3000);
+// , function () {
+//   console.log('App running on port 3000!');
+// });
